83317eebcc577f92c0a3abd7006e80a470d6decf
NaheedRayan <naheed27ray@gmail.com>
2024-09-21 01:23:28 +0600

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+st.sidebar.info(
+    """
+    - Web App URL: <https://streamlit.gishub.org>
+    - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
+    """
+)
+
+st.sidebar.title("Contact")
+st.sidebar.info(
+    """
+    Qiusheng Wu at [wetlands.io](https://wetlands.io) | [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://youtube.com/@giswqs) | [LinkedIn](https://www.linkedin.com/in/giswqs)
+    """
+)
+
+
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+st.map(df)
+
+# m = leafmap.Map(minimap_control=True)
+# m.add_basemap("OpenTopoMap")
+# # m.to_streamlit(height=600)
+# m.to_streamlit()
