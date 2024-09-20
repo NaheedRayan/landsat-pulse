011117b16f31bf5b3a376c6896dd07e56828bfe3
NaheedRayan <naheed27ray@gmail.com>
2024-09-21 01:26:08 +0600

    basic map

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,29 +5,29 @@ st.set_page_config(layout="wide")
 
 st.sidebar.info(
     """
-    - Web App URL: <https://streamlit.gishub.org>
-    - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
+    - Web App URL: <https://github.com/NaheedRayan/landsat-pulse>
+    - GitHub repository: <https://github.com/NaheedRayan/landsat-pulse>
     """
 )
 
 st.sidebar.title("Contact")
 st.sidebar.info(
     """
-    Qiusheng Wu at [wetlands.io](https://wetlands.io) | [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://youtube.com/@giswqs) | [LinkedIn](https://www.linkedin.com/in/giswqs)
+    Quantum Voyagers at [GitHub](https://github.com/NaheedRayan/landsat-pulse 
     """
 )
 
 
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=["lat", "lon"],
-)
-st.map(df)
+# df = pd.DataFrame(
+#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+#     columns=["lat", "lon"],
+# )
+# st.map(df)
 
-# m = leafmap.Map(minimap_control=True)
-# m.add_basemap("OpenTopoMap")
-# # m.to_streamlit(height=600)
-# m.to_streamlit()
+m = leafmap.Map(minimap_control=True)
+m.add_basemap("OpenTopoMap")
+# m.to_streamlit(height=600)
+m.to_streamlit()
