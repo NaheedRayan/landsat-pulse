0e0061b87f985914b241566c952b2934238047c8
NaheedRayan <naheed27ray@gmail.com>
2024-09-21 11:19:04 +0600

    new

---
+++ pages/1_📷_Notification.py
@@ -0,0 +1,25 @@
+# import ee
+import json
+import os
+import warnings
+import datetime
+# import fiona
+# import geopandas as gpd
+import folium
+import streamlit as st
+# import geemap.colormaps as cm
+# import geemap.foliumap as geemap
+from datetime import date
+# from shapely.geometry import Polygon
+
+
+
+
+def app():
+    today = date.today()
+
+    st.title("Create Satellite Timelapse")
+
+
+
+app()
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,30 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
+# Set page layout to wide
 st.set_page_config(layout="wide")
 
+# Sidebar Information
 st.sidebar.info(
     """
-    - Web App URL: <https://github.com/NaheedRayan/landsat-pulse>
-    - GitHub repository: <https://github.com/NaheedRayan/landsat-pulse>
+    - Web App URL: [Github](https://github.com/NaheedRayan/landsat-pulse)
+    - GitHub repository: [Github](https://github.com/NaheedRayan/landsat-pulse)
     """
 )
 
+# Sidebar Contact Title and Info
 st.sidebar.title("Contact")
 st.sidebar.info(
     """
-    Quantum Voyagers at [GitHub](https://github.com/NaheedRayan/landsat-pulse 
+    Quantum Voyagers at [GitHub](https://github.com/NaheedRayan/landsat-pulse)
     """
 )
 
 
-# import pandas as pd
-# import numpy as np
 
-# df = pd.DataFrame(
-#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-#     columns=["lat", "lon"],
-# )
-# st.map(df)
 
 m = leafmap.Map(minimap_control=True)
 m.add_basemap("OpenTopoMap")
-# m.to_streamlit(height=600)
+m.add_xyz_service("qms.Google Satellite Hybrid")
+
 m.to_streamlit()
